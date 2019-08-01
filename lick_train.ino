8a4649e14777c310c8f4136d590771181c7d5950
danieldkato <daniel.d.kato@gmail.com>
2019-08-01 18:24:29 -0400

    Initial commit

---
+++ lick_train.ino
@@ -0,0 +1,21 @@
+// Dispenses water every time a lick is detected. 
+// DDK 2019-07-30
+
+const int solenoid_pin = 3;
+const int lick_detector_pin = A1;
+const int thresh = 900;
+const int valve_open_time = 10; // in milliseconds
+
+void setup() {
+  // Set up pins:
+  pinMode(solenoid_pin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (abs(analogRead(lick_detector_pin)) > thresh){
+    digitalWrite(solenoid_pin, HIGH);
+    delay(valve_open_time);
+    digitalWrite(solenoid_pin, LOW);
+  }
+}
